f9a3934fc67ced11494b9f0ecedd1ff7705defe4
David Herges <dherges@users.noreply.github.com>
2026-09-08 12:15:27 +0000

    fix: avoid duplicating d.ts file emit
    Now... it is a first library build with esbuild. Okay wow... that leave's judgement calls to be made... we could emit thje d.ts files in the esbuild plugin. need to adjust output pathes for tthe .d.ts file emit... then bundling the .d.ts files needs to come after esbuild... could d.t.s bundling be a separate distinc esbuild plugin=?!?!?

--- src/lib/esbuild/angular-library-plugin.ts
+++ src/lib/esbuild/angular-library-plugin.ts
@@ -47,7 +47,9 @@ export const angularLibraryEsbuildPlugin = (options: AngularCompilerOptions): es
         writeFile: (fileName, data, writeByteOrderMark, onError, sourceFiles) => {
           if (fileName.endsWith('.d.ts') || fileName.endsWith('.d.ts.map')) {
             // .d.ts Dateien schreiben wir weiterhin für das APF auf Platte
-            baseTsHost.writeFile(fileName, data, writeByteOrderMark, onError, sourceFiles);
+            console.log("params", writeByteOrderMark, onError);
+//            baseTsHost.writeFile(fileName, data, writeByteOrderMark, onError, sourceFiles);
+// XX: skip writing the d.ts files here...but maybe we should emit them here and bundle up after esbuild???
           } else if (fileName.endsWith('.js')) {
             // Ivy-transformiertes JS wandert DIREKT in den globalen jsCache
             if (sourceFiles && sourceFiles.length > 0) {
